452f0443aa4bce616c060b80a9f698680be7e5c6
LazyGon <26216029+LazyGon@users.noreply.github.com>
2023-09-05 11:33:39 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+group = "net.okocraft"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+}
+
+tasks.compileJava {
+    options.release.set(17)
+}
+
+tasks.processResources {
+    filesMatching(listOf("plugin.yml")) {
+        expand("projectVersion" to version)
+    }
+}
---
+++ src/main/java/net/okocraft/noeasyplace/Main.java
@@ -0,0 +1,27 @@
+package net.okocraft.noeasyplace;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler(ignoreCancelled = true)
+            private void onBlockPlace(@NotNull BlockPlaceEvent event) {
+                if (event.getPlayer().getUniqueId().getMostSignificantBits() == 0L) {
+                    return;
+                }
+                if (event.getBlock().equals(event.getBlockAgainst())) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage("§ceasyplacemodeでのブロック設置は利用できません。");
+                }
+            }
+        }, this);
+    }
+
+}
